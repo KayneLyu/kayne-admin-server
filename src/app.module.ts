be2facd1435e14335472a669f6b6ea05f4bec160
Tayte Lyu <kangzaiwt@gmail.com>
2024-04-03 18:08:44 +0800

    feat: 新增person测试模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PersonModule } from './person/person.module';
 
 @Module({
-  imports: [],
+  imports: [PersonModule],
   controllers: [AppController],
   providers: [AppService],
+  exports:[AppService]
 })
 export class AppModule {}
